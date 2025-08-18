14969c62468212b53cfcaeae0e3aed0e864a79cb
Graham Jarvis <zeroThousand111@proton.me>
2025-08-18 13:17:51 +0100

    add chapter 1 to project

--- book_viewer.rb
+++ book_viewer.rb
@@ -8,3 +8,11 @@ get "/" do
 
   erb :home
 end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
+end
