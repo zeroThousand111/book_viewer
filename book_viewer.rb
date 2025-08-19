600748ee3bd5c90dd51597b0fcadb99ebfc2b025
Graham Jarvis <zeroThousand111@proton.me>
2025-08-19 17:01:32 +0100

    update book_viewer.rb to include redirect for semantically incorrect paths

--- book_viewer.rb
+++ book_viewer.rb
@@ -19,8 +19,10 @@ end
 get "/chapters/:number" do
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
-  @title = "Chapter #{number}: #{chapter_name}"
 
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
